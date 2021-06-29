575422ed0778c02e0e855543b50e74392d115e5a
jcepedaf <jcepedaf@gmail.com>
2021-06-29 21:37:57 +0200

    agrego I

---
+++ week-1/InClass/exercise-I.js
@@ -0,0 +1,16 @@
+//funcion fecha de nacimiento
+
+function nacimiento(edad){
+
+    let año = 2021;
+    let fecha = año - edad;
+    console.log(fecha);
+
+}
+
+nacimiento(30);
+nacimiento(35);
+nacimiento(30);
+
+
+
