a2da477dfd1af757f966fe87694f79da7b6ae8b9
jcepedaf <jcepedaf@gmail.com>
2021-06-29 21:30:32 +0200

    agrego H

--- week-1/InClass/exercise-F.js
+++ week-1/InClass/exercise-F.js
@@ -3,4 +3,6 @@ let profesores = 8;
 
 let total = estudiantes + profesores;
 
+console.log("Numero de estudiantes: " + estudiantes);
+console.log("Numero de profesores: " + profesores);
 console.log("El total de estudiantes mas profesores es: " + total);
---
+++ week-1/InClass/exercise-H.js
@@ -0,0 +1,12 @@
+//funcion que suma dos numeros y devuelve el valor
+
+function  sumar(a,b) {
+
+  let suma = a + b;
+  return suma;  
+
+}
+
+console.log(sumar(2,8));
+
+
