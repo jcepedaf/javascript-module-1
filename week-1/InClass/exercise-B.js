b1f10aa3d40a3575b4a112a5ce9354f1e5334610
jcepedaf <jcepedaf@gmail.com>
2021-06-29 19:05:13 +0200

    agrego ejercicio B

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,6 @@
+
+console.log("Halo, dunia! // Indonesian");
+console.log("Ciao, mondo! // Italian");
+console.log("Hola, mundo! // Spanish");
+console.log("hello, world! // EEUU");
+console.log("Salut, monde! // Francia");
