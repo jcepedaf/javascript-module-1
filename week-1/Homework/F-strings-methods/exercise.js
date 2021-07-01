6d0379131126c9afe5240fd3d70656c67218e9df
jcepedaf <jcepedaf@gmail.com>
2021-07-01 17:44:30 +0200

    agrego ejercicio 1

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,11 @@
 // Start by creating a variable `message`
 
-console.log(message);
+//declaro variable nombre y nombreLongitud
+var nombre = "Jesús"; //agrego el valor de mi nombre
+var nombreLongitud = nombre.length;  //utilizo la funcion length para saber la cantidad de caracteres de mi nombre
+
+// declaro la variable message
+
+var message = `Mi nombre es ${nombre} y mi nombre tiene ${nombreLongitud} caracteres de longitud`; // agrego el valor de la variable
+
+console.log(message); // imprimo en pantalla el valor de message
