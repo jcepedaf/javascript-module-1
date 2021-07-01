1e40c8c56813839045e041e4e0c89e4689d8c742
jcepedaf <jcepedaf@gmail.com>
2021-07-01 19:12:53 +0200

    agrego ejercicio 4 de parametros de funciones

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -2,4 +2,9 @@
 
 // Call the function and assign to a variable `sum`
 
-console.log(sum);
+function sum(a,b){//declaro funcion de suma a + b que recibe esos mismos parametros
+
+    return a+b; //retorno resultado de suma a + b
+}
+
+console.log(sum(13,124));//imprimo resultado de la funcion sum que recibe los parametros 13 y 124
