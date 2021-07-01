7a2400b23e1136dae8a7f31e7ddd393e0cb06b10
jcepedaf <jcepedaf@gmail.com>
2021-07-01 19:01:42 +0200

    agrego ejercicio 1 de parametros de funciones

--- week-1/Homework/K-functions-parameters/exercise.js
+++ week-1/Homework/K-functions-parameters/exercise.js
@@ -1,9 +1,10 @@
 // Complete the function so that it takes input parameters
-function multiply() {
+function multiply(a,b) {//agrego parametros de entrada a la funcion llamados a y b
   // Calculate the result of the function and return it
+  return a*b; //resalizo multiplicacion de a*b y devuelvo el resultado
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+var result = multiply(3, 4); //llamo la funcion multiply con valor de parametro 3 y 4 y el resultado se lo asigno a la variable result
 
-console.log(result);
+console.log(result);//imprimo el valor de result en pantalla
