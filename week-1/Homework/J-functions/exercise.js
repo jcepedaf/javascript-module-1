9c54d44d0ec902c330950bf7af1e171195e7784e
jcepedaf <jcepedaf@gmail.com>
2021-07-01 18:34:42 +0200

    agrego ejercicio 1 de funciones

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,17 @@
 function halve(number) {
   // complete the function here
+  var resultado = number/2; //declaro variable resultado y agrego el valor de la mitad del varlor de numero
+  return resultado;  //devuelvo el valor de resultado
 }
 
-var result = halve(12);
-
-console.log(result);
+console.log("para valor de numero 12 la mitad es:")//imprimo mensaje de cual debe ser el resultado de la mitad del numero
+var result = halve(12); // declaro la variable result y le agrego el valor de -> la funcion halve con valor de numero 12
+console.log(result); // imprimo el resultado
+//repito para otro valor de numero
+console.log("para valor de numero 100 la mitad es:")//imprimo mensaje de cual debe ser el resultado de la mitad del numero
+var result = halve(100); // declaro la variable result y le agrego el valor de -> la funcion halve con valor de numero 100
+console.log(result); // imprimo el resultado
+//repito
+console.log("para valor de numero 78 la mitad es:")//imprimo mensaje de cual debe ser el resultado de la mitad del numero
+var result = halve(78); // declaro la variable result y le agrego el valor de -> la funcion halve con valor de numero 78
+console.log(result); // imprimo el resultado
