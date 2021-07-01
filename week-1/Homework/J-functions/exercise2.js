1d403005ac53edf481c35c87b1407e604f61abe5
jcepedaf <jcepedaf@gmail.com>
2021-07-01 18:55:52 +0200

    agrego ejercicio 2 de funciones

--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,9 @@
 function triple(number) {
   // complete function here
+  var resultado = number*3; //multiplico por tres la entrada de la funcion y el valor se lo agrego a la varible resultado
+  return resultado;//retorno el resultado
 }
 
-var result = triple(12);
+var result = triple(12); //declaro la variable result y le agrego el valor de la funcion triple con entrada de 12
 
-console.log(result);
+console.log(result); //imprimo result
