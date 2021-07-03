7741c8105f4a861a3ae79c6691ced58bbc03cbc4
jcepedaf <jcepedaf@gmail.com>
2021-07-03 12:53:32 +0200

    agrego ejercicio 5 de Extra

--- week-1/Extra/5-currency-conversion.js
+++ week-1/Extra/5-currency-conversion.js
@@ -5,7 +5,11 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(a) {
+
+  b = a*1.4;
+  return b;
+}
 
 /*
   CURRENCY FORMATTING
@@ -15,7 +19,12 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(a) {
+  a= a-(a*0.01);
+  b= a*5.7;
+ // b= b.toFixed(2)
+  return b;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
