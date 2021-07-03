1d3b22e043794c4e644622cab6e7845081bfbdcc
jcepedaf <jcepedaf@gmail.com>
2021-07-03 10:40:13 +0200

    agrego cambio a ejercicio 5 de K-funtion

--- week-1/Homework/K-functions-parameters/exercise5.js
+++ week-1/Homework/K-functions-parameters/exercise5.js
@@ -1,5 +1,16 @@
 // Declare your function here
 
-const greeting = createLongGreeting("Daniel", 30);
+function createGreeting(nombre,edad){ //declaro funcion
+    var resultado = `Hola mi nombre es ${nombre} y tengo ${edad} años` ; //declaro variable y le asigno un valor mas la entrada del parametro nombre y edad
+    return resultado;//devuelvo resultado
+}
 
-console.log(greeting);
+const greeting = createGreeting("Daniel",30);//llamo la funcion y el resultado se lo asigno a greeting
+
+console.log(greeting);//imprimo greeting
+
+
+
+
+
+
