12f67e0d1163829de17daf9fdaac14084755a953
jcepedaf <jcepedaf@gmail.com>
2021-07-06 21:23:12 +0200

    agrego ejercicio H de Inclass week-2

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,12 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for (let a=5; a <= 20 ; a++){
+  if(isEven(a)) {
+  
+
+  console.log("The exponential of " + a +" is " + exponential(a));
+
+  }
+}
