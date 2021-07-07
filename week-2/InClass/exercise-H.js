80ffcd799f8a9a4eaf8ce900139fc71472690358
jcepedaf <jcepedaf@gmail.com>
2021-07-07 18:17:02 +0200

    agrego ejercicio H de Inclass

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -1,16 +1,13 @@
-function exponential(number) {
+function exponential(number) { //funcion que calcula el exponecial de un numero
   return number * number;
 }
 
-function isEven(number) {
+function isEven(number) { // funcion que devuelve un numero si este es par
   return number % 2 === 0;
 }
 
-for (let a=5; a <= 20 ; a++){
-  if(isEven(a)) {
-  
-
-  console.log("The exponential of " + a +" is " + exponential(a));
-
+for (let a = 5; a <= 20; a++) { // funcion que itera o cuenta entre 5 y 20
+  if (isEven(a)) {
+    console.log("The exponential of " + a + " is " + exponential(a));
   }
 }
