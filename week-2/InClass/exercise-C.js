ec1a3d43d769000fc83ef75259b5e2a08fb2503d
jcepedaf <jcepedaf@gmail.com>
2021-07-07 17:50:32 +0200

    agrego ejercicio C de Inclass

--- week-2/InClass/exercise-C.js
+++ week-2/InClass/exercise-C.js
@@ -1,3 +1,5 @@
+let n = 20;
+
 function numberChecker(num) {
   if (num > 20) {
     return `${num} is greater than 20`;
@@ -9,3 +11,9 @@ function numberChecker(num) {
     return `${num} isn't even a number :(`;
   }
 }
+
+console.log(numberChecker(n));
+
+//Este codigo lo que hace es comparar si el parametro num de la funcion numberChecker es mayor que 20, igual que 20,
+// menor que 20 y si no es un numero y imprime en pantalle el resultado de la comparacion
+//para el ejemplo de n=20 el resultado es: 20 is equal to 20
