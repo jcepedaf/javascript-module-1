2e571e816d8ef12136accc9e6eb8d101ef310218
jcepedaf <jcepedaf@gmail.com>
2021-07-07 18:27:09 +0200

    agrego ejercicio I de Inclass

---
+++ week-2/InClass/exercise-I.js
@@ -0,0 +1,12 @@
+//En el node REPL, ingresa el siguiente arreglo:
+//const fruits = ['banana', 'apple', 'strawberry', 'kiwi', 'fig', 'orange'];
+//Ahora, usando los índices correctos, obtén los siguientes valores:
+//pruebo fruits[coloco indice de 0 a 5] y imprime en pantalla el valor que correponde al indice ejemplo para el indice 0 imprime banana
+//para el indici 5 imprime orange
+//strawberry
+//kiwi
+//orange
+//banana
+//Luego, reemplaza 'apple' con 'raspberry', y reemplaza 'fig' con 'pineapple'.
+//asinno el valor a fruits[1]='rasperry' y fruits[4]='pineapple'
+//fruits queda -> [ 'banana', 'rasperry', 'strawberry', 'kiwi', 'pineapple', 'orange' ]
