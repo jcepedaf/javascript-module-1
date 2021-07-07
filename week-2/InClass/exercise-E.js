35dd19491e34d4d0d54cc56e3f784a6a996a1a83
jcepedaf <jcepedaf@gmail.com>
2021-07-07 18:11:15 +0200

    agrego ejercicio E de Inclass

---
+++ week-2/InClass/exercise-E.js
@@ -0,0 +1,11 @@
+//Escribe las siguientes expresiones en node REPL y ve el resultado. Encuentras algún valor inesperado?
+//let num = 10 -> undefined
+//num > 5 && num < 15 -> true
+//num < 10 || num === 10 -> true
+//false || true -> true
+// !true -> false
+//let greaterThan5 = num > 5 -> undefined
+//!greaterThan5 -> false
+//!(num === 10) -> false
+
+//no vi algun valor inesperado
