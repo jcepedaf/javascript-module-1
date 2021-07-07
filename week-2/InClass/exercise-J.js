f8bfaaafc7ae6b521b71276c23a6f77f5ea1a1c0
jcepedaf <jcepedaf@gmail.com>
2021-07-07 19:33:41 +0200

    agrego ejercicio J de Inclass

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,12 @@
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1] == "Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
-}
+ }
+ let names = ["Alex", "Amara", "Carlos"];
+ let names2 = ["Ali", "Amy", "Naresh"];
+ 
+ const result = secondMatchesAmy(names2);
+ console.log(result)
+ 
