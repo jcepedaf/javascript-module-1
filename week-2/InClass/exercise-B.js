ae9e37d6d6a1d938ee903ef3878bf78ac853ed35
jcepedaf <jcepedaf@gmail.com>
2021-07-07 17:44:44 +0200

    agrego ejercicio  de Inclass

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -1,9 +1,14 @@
+//actividad 1: en node REPL, cuale es el resultado de aplicar el operador typeof a true o false
+//resultado: en ambos caso devuelve -> boolean
+
+//actividad 2: corregir
+
 function boolChecker(bool) {
-  if (typeof bool === ) {
+  if (typeof bool === "boolean") {
     return "You've given me a bool, thanks!";
   }
 
-  return "No bool, not cool.";
+  return `No bool, not cool. ${typeof bool}`;
 }
 
-boolChecker(true);
+console.log(boolChecker(1==1));
