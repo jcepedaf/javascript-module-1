32bb74869988fd8ff49b61efffbcecec9cabc31a
jcepedaf <jcepedaf@gmail.com>
2021-07-07 22:23:40 +0200

    agrego homework F ejercicio 2

--- week-2/Homework/F-logical-operators/exercise2.js
+++ week-2/Homework/F-logical-operators/exercise2.js
@@ -5,7 +5,38 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(num) {
+  if (num < 0) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+function isBetween5and10(num) {
+  if (num=>5&&num<=10){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function isShortName(array){
+  if (array.length<7){
+  return true;
+} else {
+  return false;
+}
+}
+
+function startsWithD(array) {
+  if(array[0]==="D"){
+    return true;
+  }else {
+    return false;
+  }
+}
+
 
 /* 
   DO NOT EDIT BELOW THIS LINE
