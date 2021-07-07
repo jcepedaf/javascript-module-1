c29e4eab5acb80215415833857dcb17541df972c
jcepedaf <jcepedaf@gmail.com>
2021-07-07 17:38:19 +0200

    agrego ejercicio A de Inclass

---
+++ week-2/InClass/exercise-A.js
@@ -0,0 +1,14 @@
+//Se abre el terminal de node
+// y se ejecuta:
+
+//1+2
+//resultado: 3
+
+//"hello"
+//resultado: 'hello'
+
+//let favouriteColor = "purple"
+//resultado: undefined
+
+//console.log(favouriteColour)
+//resultado: purple y undefined
