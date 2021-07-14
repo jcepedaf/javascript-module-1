a4ac37225f47e9edba3887d097cbdebf001bf12e
jcepedaf <jcepedaf@gmail.com>
2021-07-14 21:05:00 +0200

    agrego ejercicio G de inClass

---
+++ week-3/InClass/exercise-G.js
@@ -0,0 +1,2 @@
+let fechasNacimiento = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
+console.log("These are the birth years of people who can drive: "+(fechasNacimiento.filter(name => 2021-name >= 17).join(" ,")));
